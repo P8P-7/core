d21712fea86c8fec65c0f40c2900f1dcda8da5ee
Amorandron <jan@janwilts.com>
2018-05-01 16:54:21 +0200

    Command structure should be pretty much done, only testing left

--- src/controller/command_runner.cpp
+++ src/controller/command_runner.cpp
@@ -2,7 +2,7 @@
 
 using namespace goliath::commands;
 
-void command_runner::run(const unsigned command_id) const {
+void command_runner::run(const unsigned command_id) {
     std::shared_ptr<Command> instance;
 
     auto instance_ptr = cm.lock()->get_instance(command_id);
@@ -12,20 +12,38 @@ void command_runner::run(const unsigned command_id) const {
     else
         return;
 
+    std::thread(&command_runner::execute, this, std::ref(command_id), instance);
+}
+
+void command_runner::execute(const unsigned &command_id, std::shared_ptr<Command> instance) {
+    thread_count++;
+
     while(true) {
         if(can_start(*instance)) {
             cm.lock()->set_command_status(command_id, command_status::RUNNING);
             instance->execute();
             cm.lock()->set_command_status(command_id, command_status::STALE);
+            break;
         }
+        else {
+            for(unsigned handle : instance->get_handles()) {
+                unsigned locker = hm.get(handle)->get_locker();
+                cm.lock()->get_instance(locker).lock()->interrupt();
+            }
+        }
+
+        std::unique_lock<std::mutex> lock(mutex);
+        var->wait(lock);
     }
+
+    thread_count--;
 }
 
 bool command_runner::can_start(const Command& command) const {
     bool start = true;
 
     for(const unsigned handle : command.get_handles())
-        if(hm.lock()->get(handle)->get_status())
+        if(hm.get(handle)->get_status())
             start = false;
 
     return start;
--- src/controller/command_runner.h
+++ src/controller/command_runner.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <mutex>
 #include <thread>
 
 #include "commands/command_map.h"
@@ -10,14 +11,20 @@ using namespace goliath::handles;
 namespace goliath::commands {
     class command_runner {
     public:
-        command_runner(std::shared_ptr<command_map> cm, std::shared_ptr<handle_map> hm)
-                : cm(cm), hm(hm) {}
+        command_runner(std::shared_ptr<command_map> cm, std::shared_ptr<std::condition_variable> var)
+                : cm(cm), hm(var), var(var) {}
 
-        void run(const unsigned command_id) const;
+        void run(const unsigned command_id);
+        void execute(const unsigned &command_id, std::shared_ptr<Command> instance);
         bool can_start(const Command &command) const;
 
     private:
+        std::atomic<int> thread_count{0};
+
         std::weak_ptr<command_map> cm;
-        std::weak_ptr<handle_map> hm;
+        handle_map hm;
+
+        std::mutex mutex;
+        std::shared_ptr<std::condition_variable> var;
     };
 }
