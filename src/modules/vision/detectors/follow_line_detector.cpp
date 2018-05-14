aa33fc996b58225dd0adb77c98f2de34ef0b952c
Amorandron <jan@janwilts.com>
2018-05-14 13:42:06 +0200

    Added some documentation

--- src/modules/vision/detectors/follow_line_detector.cpp
+++ src/modules/vision/detectors/follow_line_detector.cpp
@@ -3,19 +3,18 @@
 using namespace goliath::vision;
 using namespace goliath::exceptions;
 
-follow_line_detector::follow_line_detector(const cv::Mat& input, int boxes, int box_height, int boxes_bottom_padding,
-                                           int boxes_horizontal_padding)
-        : detector(input), boxes(boxes), box_height(box_height), boxes_bottom_padding(boxes_bottom_padding),
-          boxes_horizontal_padding(boxes_horizontal_padding) {
+follow_line_detector::follow_line_detector(const cv::Mat& input, int boxes, int box_height, int boxes_bottom_margin,
+                                           int boxes_horizontal_margin)
+        : detector(input), boxes(boxes), box_height(box_height), boxes_bottom_margin(boxes_bottom_margin),
+          boxes_horizontal_margin(boxes_horizontal_margin) {
     if(boxes > 4 || boxes < 0) {
         throw vision_error("The max amount of ROI boxes must be between 1 and 4");
     }
 }
 
-follow_line_detector::follow_line_detector(const follow_line_detector& other, int boxes, int box_height,
-                                           int boxes_bottom_padding, int boxes_horizontal_padding)
+follow_line_detector::follow_line_detector(const follow_line_detector& other)
         : detector(other.input), boxes(other.boxes), box_height(other.box_height),
-          boxes_bottom_padding(other.boxes_bottom_padding), boxes_horizontal_padding(other.boxes_horizontal_padding) {
+          boxes_bottom_margin(other.boxes_bottom_margin), boxes_horizontal_margin(other.boxes_horizontal_margin) {
 }
 
 std::vector<cv::Vec4d> follow_line_detector::detect() const {
@@ -28,9 +27,9 @@ std::vector<cv::Vec4d> follow_line_detector::detect() const {
     int output_boxes = 0;
 
     for(int box_index = 0; box_index < boxes; ++box_index) {
-        const int x = boxes_horizontal_padding;
-        const int y = boxes_bottom_padding + box_height * (box_index + 1);
-        const int w = (input.cols - 1) - (boxes_horizontal_padding * 2);
+        const int x = boxes_horizontal_margin;
+        const int y = boxes_bottom_margin + box_height * (box_index + 1);
+        const int w = (input.cols - 1) - (boxes_horizontal_margin * 2);
         const int h = box_height;
         roi_processor roi_processor(input, x, y, w, h);
         cv::Mat box = roi_processor.process();
--- src/modules/vision/detectors/follow_line_detector.h
+++ src/modules/vision/detectors/follow_line_detector.h
@@ -6,7 +6,16 @@
 #include "../util/vision_error.h"
 #include "../processors/roi_processor.h"
 
+/**
+ * @file follow_line_detector.h
+ * @author Group 7 - Informatica
+ */
+
 namespace goliath::vision {
+    /**
+     * @enum goliath::vision::follow_line_direction
+     * @brief Represents a direction the robot should go in, outputted from the follow_line_detector.
+     */
     enum class follow_line_direction {
         ON_COURSE = 0,
         LEFT = 1,
@@ -14,23 +23,35 @@ namespace goliath::vision {
         ERROR = 3
     };
 
+    /**
+     * @class goliath::vision::follow_line_detector
+     * @brief Takes in an input image, splits it up into "boxes". Within those boxes it checks if there is a single
+     * contour, if so take the center of that contour and compare it to the center of the box. This will return an
+     * offset. Take the average of all offsets to determine if the robot should go straight, left or right.
+     */
     class follow_line_detector : public detector {
     public:
+         /**
+          * @param input Input Image
+          * @param boxes Amount of boxes to create a ROI from
+          * @param box_height The height of each of the boxes
+          * @param boxes_bottom_margin Margin to start bottom-most box from
+          * @param boxes_horizontal_margin Margins on both sides of the boxes
+          */
+        follow_line_detector(const cv::Mat &input, int boxes, int box_height, int boxes_bottom_margin,
+                             int boxes_horizontal_margin);
         /**
-         * @param input Input Image
-         * @param boxes Amount of ROI's to check
+         * @brief Copy constructor
          */
-        follow_line_detector(const cv::Mat &input, int boxes, int box_height, int boxes_bottom_padding,
-                             int boxes_horizontal_padding);
-        follow_line_detector(const follow_line_detector &other, int boxes, int box_height, int boxes_bottom_padding,
-                             int boxes_horizontal_padding);
+        follow_line_detector(const follow_line_detector &other);
 
         /**
-         * @return Only value [0] has values. Containing a offset from the center for each of the boxes
+         * @return Only value [0][0] has a value. Containing a value whether the robot should continue going straight,
+         * or go left/right. This value is defined in the follow_line_direction enum
          */
         std::vector<cv::Vec4d> detect() const override;
 
     private:
-        const int boxes, box_height, boxes_bottom_padding, boxes_horizontal_padding;
+        const int boxes, box_height, boxes_bottom_margin, boxes_horizontal_margin;
     };
 }
