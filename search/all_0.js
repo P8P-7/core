efabac830fd4f8b63b4ccce5407ed46aac47cab5
Travis CI <travis@travis-ci.org>
2018-05-18 15:16:36 +0000

    Deploy Goliath docs to GitHub Pages. Build: 85
    Commit: 144c637eb35dd5696f74f48450558d4ff4c8017d

--- search/all_0.js
+++ search/all_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['add',['add',['../classgoliath_1_1commands_1_1CommandMap.html#a05e662fada42a195d1064365004c9706',1,'goliath::commands::CommandMap::add()'],['../classgoliath_1_1handles_1_1HandleMap.html#a735a565b8fdb8fe861dc6507377c49d4',1,'goliath::handles::HandleMap::add()']]],
+  ['add',['add',['../classgoliath_1_1commands_1_1CommandMap.html#a24b7eaaf96c3fca72d995ef3d457d69e',1,'goliath::commands::CommandMap::add(std::shared_ptr&lt; Command &gt; command)'],['../classgoliath_1_1commands_1_1CommandMap.html#a31b84a2b440ba2a71036ab8fe9be8219',1,'goliath::commands::CommandMap::add(size_t commandId, Targs...args)'],['../classgoliath_1_1handles_1_1HandleMap.html#a735a565b8fdb8fe861dc6507377c49d4',1,'goliath::handles::HandleMap::add()']]],
   ['address',['address',['../classgoliath_1_1messaging_1_1ZmqIo.html#a987503917974ed9aa830424fa3bb3ca6',1,'goliath::messaging::ZmqIo']]]
 ];
