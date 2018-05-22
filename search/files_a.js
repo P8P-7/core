8497c723533878a947b9f7560189704baf6bb85d
Travis CI <travis@travis-ci.org>
2018-05-22 11:55:52 +0000

    Deploy Goliath docs to GitHub Pages. Build: 98
    Commit: e0a95cc7637c4c30b39801ad5c16f94c37ea8f59

--- search/files_a.js
+++ search/files_a.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['processor_2eh',['processor.h',['../processor_8h.html',1,'']]],
-  ['proto_5frepository_2eh',['proto_repository.h',['../proto__repository_8h.html',1,'']]],
   ['publisher_5fservice_2eh',['publisher_service.h',['../publisher__service_8h.html',1,'']]]
 ];
