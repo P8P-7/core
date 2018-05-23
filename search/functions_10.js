57bb66f138cda5254e1f5195872bb14e0080ef35
Travis CI <travis@travis-ci.org>
2018-05-23 11:57:04 +0000

    Deploy Goliath docs to GitHub Pages. Build: 106
    Commit: 4f57af9fe731b0dbab08836bef099bae50952a10

--- search/functions_10.js
+++ search/functions_10.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['unlock',['unlock',['../classgoliath_1_1handles_1_1Handle.html#a4c9f0ac77380e119586aa13f8ea0a18e',1,'goliath::handles::Handle']]]
+  ['unlock',['unlock',['../classgoliath_1_1handles_1_1Handle.html#a4c9f0ac77380e119586aa13f8ea0a18e',1,'goliath::handles::Handle']]],
+  ['unlockall',['unlockAll',['../classgoliath_1_1handles_1_1HandleMap.html#ad2134b706b53296656af2ad34f78d57b',1,'goliath::handles::HandleMap']]]
 ];
