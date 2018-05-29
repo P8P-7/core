a9c913666a332548077323b47014cd07230b33cd
Travis CI <travis@travis-ci.org>
2018-05-29 13:12:18 +0000

    Deploy Goliath docs to GitHub Pages. Build: 141
    Commit: 894de18d5817ca5756233f1ab6efd4b2ce469ed9

--- search/classes_5.js
+++ search/classes_5.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['gpio',['GPIO',['../classgoliath_1_1gpio_1_1GPIO.html',1,'goliath::gpio']]],
-  ['gpiotranslator',['GPIOTranslator',['../structGPIOTranslator.html',1,'']]]
+  ['gpio',['GPIO',['../classgoliath_1_1gpio_1_1GPIO.html',1,'goliath::gpio']]]
 ];
