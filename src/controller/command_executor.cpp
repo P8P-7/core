f611cc0d9feb832fab02f651ec3c36cdd15f8d37
ObeA <obe@obeattema.nl>
2018-05-29 14:05:00 +0200

    Temporary fix running out of threads

--- src/controller/command_executor.cpp
+++ src/controller/command_executor.cpp
@@ -4,16 +4,12 @@
 using namespace goliath::commands;
 
 CommandExecutor::CommandExecutor(CommandMap &commands, HandleMap &handles)
-        : commands(commands), handles(handles) {
-}
+        : commands(commands), handles(handles) { }
 
-CommandExecutor::~CommandExecutor() {
-    for (auto &thread : threads) {
-        thread.join();
-    }
-}
+CommandExecutor::~CommandExecutor() { }
 
 void CommandExecutor::run(const size_t commandId, const CommandMessage &message) {
+    std::lock_guard<std::mutex> lock(mutex);
     if (!commands.commandExists(commandId)) {
         throw std::runtime_error("Command does not exist"); // TODO: Own exception class?
     }
@@ -25,7 +21,8 @@ void CommandExecutor::run(const size_t commandId, const CommandMessage &message)
     }
     item.status = CommandStatus::STARTING;
 
-    threads.emplace_back(&CommandExecutor::tryExecute, this, commandId, message);
+    std::thread thread(&CommandExecutor::tryExecute, this, commandId, message);
+    thread.detach();
 }
 
 void CommandExecutor::tryExecute(const size_t &commandId, const CommandMessage &message) {
@@ -34,18 +31,20 @@ void CommandExecutor::tryExecute(const size_t &commandId, const CommandMessage &
     CommandItem &item = commands[commandId];
     auto requiredHandles = handles.getHandles(item.instance->getRequiredHandles());
     if (canStart(*(item.instance))) {
-        lock.unlock();
-
         requiredHandles.lockAll(commandId);
         item.status = CommandStatus::STARTED;
+        lock.unlock();
 
         BOOST_LOG_TRIVIAL(info) << "Execution of \"Command " << commandId << "\" has started";
-        item.instance->run(requiredHandles, message);
+        try {
+            item.instance->run(requiredHandles, message);
+        } catch (std::exception& ex) {
+            BOOST_LOG_TRIVIAL(fatal) << "Error executing command \"" << commandId << "\" what(): " << ex.what();
+        }
         BOOST_LOG_TRIVIAL(info) << "Execution of \"Command " << commandId << "\" has finished";
 
-        requiredHandles.unlockAll();
-
         lock.lock();
+        requiredHandles.unlockAll();
         item.status = CommandStatus::STALE;
         return;
     }
@@ -57,9 +56,19 @@ void CommandExecutor::tryExecute(const size_t &commandId, const CommandMessage &
         requiredHandles[handleId]->waitAndLock(commandId);
     }
 
-    item.status = CommandStatus::STARTED;
-    item.instance->run(requiredHandles, message);
     lock.lock();
+    item.status = CommandStatus::STARTED;
+    lock.unlock();
+    BOOST_LOG_TRIVIAL(info) << "* Execution of \"Command " << commandId << "\" has started";
+    try {
+        item.instance->run(requiredHandles, message);
+    } catch (std::exception& ex) {
+        BOOST_LOG_TRIVIAL(fatal) << "Error executing command \"" << commandId << "\" what(): " << ex.what();
+    }
+    BOOST_LOG_TRIVIAL(info) << "* Execution of \"Command " << commandId << "\" has finished";
+
+    lock.lock();
+    requiredHandles.unlockAll();
     item.status = CommandStatus::STALE;
 }
 
