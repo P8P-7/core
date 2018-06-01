9134a868d5115242fc9571bc4d06270359394cbc
Amorandron <jan@janwilts.com>
2018-06-01 12:59:32 +0200

    Cleaned up pows

--- src/modules/transitions/src/methods/eases.cpp
+++ src/modules/transitions/src/methods/eases.cpp
@@ -1,7 +1,9 @@
 #include <goliath/transitions/methods/eases.h>
 
+#include <cmath>
+
 double goliath::transitions::methods::easeInQuad(double value) {
-    return value * value;
+    return std::pow(value, 2);
 }
 
 double goliath::transitions::methods::easeOutQuad(double value) {
@@ -9,42 +11,41 @@ double goliath::transitions::methods::easeOutQuad(double value) {
 }
 
 double goliath::transitions::methods::easeInOutQuad(double value) {
-    return value < 0.5 ? 2 * value * value : -1 + (4 - 2 * value) * value;
+    return value < 0.5 ? 2 * std::pow(value, 2) : -1 + (4 - 2 * value) * value;
 }
 
 double goliath::transitions::methods::easeInCubic(double value) {
-    return value * value * value;
+    return std::pow(value, 3);
 }
 
 double goliath::transitions::methods::easeOutCubic(double value) {
-    return (value - 1) * value * value + 1;
+    return (value - 1) * std::pow(value, 2) + 1;
 }
 
 double goliath::transitions::methods::easeInOutCubic(double value) {
-    return value < 0.5 ? 4 * value * value * value : (value - 1) * (2 * value - 2) * (2 * value - 2) + 1;
+    return value < 0.5 ? 4 * std::pow(value, 3) : (value - 1) * (2 * value - 2) * (2 * value - 2) + 1;
 }
 
 double goliath::transitions::methods::easeInQuart(double value) {
-    return value * value * value * value;
+    return std::pow(value, 4);
 }
 
 double goliath::transitions::methods::easeOutQuart(double value) {
-    return 1 - (value - 1) * value * value * value;
+    return 1 - (value - 1) * std::pow(value, 3);
 }
 
 double goliath::transitions::methods::easeInOutQuart(double value) {
-    return value < 0.5 ? 8 * value * value * value * value : 1 - 8 * (value - 1) * value * value * value;
+    return value < 0.5 ? 8 * std::pow(value, 4) : 1 - 8 * (value - 1) * std::pow(value, 3);
 }
 
 double goliath::transitions::methods::easeInQuint(double value) {
-    return value * value * value * value * value;
+    return std::pow(value, 5);
 }
 
 double goliath::transitions::methods::easeOutQuint(double value) {
-    return 1 + (value - 1) * value * value * value * value;
+    return 1 + (value - 1) * std::pow(value, 4);
 }
 
 double goliath::transitions::methods::easeInOutQuint(double value) {
-    return value < 0.5 ? 16 * value * value * value * value * value : 1 +
-                                                                      16 * (value - 1) * value * value * value * value;
+    return value < 0.5 ? 16 * std::pow(value, 5) : 1 + 16 * (value - 1) * std::pow(value, 4);
 }
