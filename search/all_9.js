d71e3edb2276a908490a01d5c58d61a2235e47eb
Travis CI <travis@travis-ci.org>
2018-06-05 09:49:40 +0000

    Deploy Goliath docs to GitHub Pages. Build: 201
    Commit: 73a39d96bfc820973d7869fc62edc742348a6635

--- search/all_9.js
+++ search/all_9.js
@@ -9,5 +9,6 @@ var searchData=
   ['linedetector',['LineDetector',['../classgoliath_1_1vision_1_1LineDetector.html',1,'goliath::vision::LineDetector'],['../classgoliath_1_1vision_1_1LineDetector.html#ab129d04b18f54f9f50a7855167f5e06d',1,'goliath::vision::LineDetector::LineDetector()']]],
   ['lock',['lock',['../classgoliath_1_1handles_1_1Handle.html#ada0713e8f0701f4a295d009b4d26125e',1,'goliath::handles::Handle']]],
   ['lockall',['lockAll',['../classgoliath_1_1handles_1_1HandleMap.html#a9730968ec7f3a01f8b423bc18afcdcbe',1,'goliath::handles::HandleMap']]],
+  ['logrepository',['LogRepository',['../classgoliath_1_1repositories_1_1LogRepository.html',1,'goliath::repositories']]],
   ['longestlines',['longestLines',['../classgoliath_1_1vision_1_1LineDetector.html#a844c5269aeb5c543031b605048f732a7',1,'goliath::vision::LineDetector']]]
 ];
--- search/classes_9.js
+++ search/classes_9.js
@@ -3,5 +3,6 @@ var searchData=
   ['ledstatus',['LedStatus',['../structgoliath_1_1controller_1_1LedStatus.html',1,'goliath::controller']]],
   ['ledstripcontroller',['LedStripController',['../classgoliath_1_1controller_1_1LedStripController.html',1,'goliath::controller']]],
   ['linedancecommand',['LineDanceCommand',['../classgoliath_1_1commands_1_1LineDanceCommand.html',1,'goliath::commands']]],
-  ['linedetector',['LineDetector',['../classgoliath_1_1vision_1_1LineDetector.html',1,'goliath::vision']]]
+  ['linedetector',['LineDetector',['../classgoliath_1_1vision_1_1LineDetector.html',1,'goliath::vision']]],
+  ['logrepository',['LogRepository',['../classgoliath_1_1repositories_1_1LogRepository.html',1,'goliath::repositories']]]
 ];
