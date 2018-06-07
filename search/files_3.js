08e52e29dc2bf55d02ff9b89f27ce93dc137c772
Travis CI <travis@travis-ci.org>
2018-06-07 15:04:39 +0000

    Deploy Goliath docs to GitHub Pages. Build: 222
    Commit: aed42638092e1e035a33c012e7d6e17df0e688ca

--- search/files_3.js
+++ search/files_3.js
@@ -4,5 +4,6 @@ var searchData=
   ['emotion_5fhandle_2eh',['emotion_handle.h',['../emotion__handle_8h.html',1,'']]],
   ['emotion_5fpublisher_2eh',['emotion_publisher.h',['../emotion__publisher_8h.html',1,'']]],
   ['emotion_5frepository_2eh',['emotion_repository.h',['../emotion__repository_8h.html',1,'']]],
-  ['emotions_2eh',['emotions.h',['../emotions_8h.html',1,'']]]
+  ['emotions_2eh',['emotions.h',['../emotions_8h.html',1,'']]],
+  ['enter_5fcommand_2eh',['enter_command.h',['../enter__command_8h.html',1,'']]]
 ];
