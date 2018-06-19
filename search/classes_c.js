c337ade522e5b698b5c06e61441e102df657036f
Travis CI <travis@travis-ci.org>
2018-06-19 11:14:10 +0000

    Deploy Goliath docs to GitHub Pages. Build: 290
    Commit: 1556aeaeeef9f97715056b97dc7e7f7e9eec8e44

--- search/classes_c.js
+++ search/classes_c.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['parallelreel',['ParallelReel',['../classgoliath_1_1transitions_1_1ParallelReel.html',1,'goliath::transitions']]],
   ['phase',['Phase',['../classgoliath_1_1transitions_1_1Phase.html',1,'goliath::transitions']]],
+  ['pollingrepository',['PollingRepository',['../classgoliath_1_1repositories_1_1PollingRepository.html',1,'goliath::repositories']]],
   ['processor',['Processor',['../classgoliath_1_1vision_1_1Processor.html',1,'goliath::vision']]],
   ['protorepository',['ProtoRepository',['../classgoliath_1_1repositories_1_1ProtoRepository.html',1,'goliath::repositories']]],
   ['publisherservice',['PublisherService',['../classgoliath_1_1foundation_1_1PublisherService.html',1,'goliath::foundation']]]
