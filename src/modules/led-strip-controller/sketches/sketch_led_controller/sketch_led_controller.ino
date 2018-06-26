15066536ea8e61ec4b6f83bdc44dd135055fe5d1
ObeA <obe@obeattema.nl>
2018-06-26 11:04:24 +0200

    Add support for multiple led strips
    Squashed commit of the following:
    
    commit 27537898c9ade1cc9f134d69b6d17044597bdeac
    Merge: a585dd9 3aff7b2
    Author: Amorandron <jan@janwilts.com>
    Date:   Tue Jun 26 08:25:36 2018 +0200
    
        Merge branch 'master' into multiple-led-strips
    
    commit a585dd95780b3bab1739793ebc76f746f1bc25e9
    Author: Amorandron <jan@janwilts.com>
    Date:   Tue Jun 26 08:25:31 2018 +0200
    
        update submodules
    
    commit 66bb35fd7172ae90f021389428c0481885cf5972
    Merge: 43c07f9 b5d41cf
    Author: Aran Kieskamp <aranna00@gmail.com>
    Date:   Mon Jun 25 09:27:42 2018 +0200
    
        Merge remote-tracking branch 'origin/multiple-led-strips' into multiple-led-strips
    
    commit 43c07f93f53c95d881bc5c14374573526c6063f6
    Author: Aran Kieskamp <aranna00@gmail.com>
    Date:   Mon Jun 25 09:25:56 2018 +0200
    
        changed number of pixels
        updated test
    
    commit 1828492c67beac12a10c883c55b388942713dfbe
    Author: Aran Kieskamp <aranna00@gmail.com>
    Date:   Fri Jun 22 16:55:00 2018 +0200
    
        updated arduino sketch with support for 3 LED strips
    
    commit fd17029f3f6fc30f4f5958bf0035ec8677e99183
    Author: Aran Kieskamp <aranna00@gmail.com>
    Date:   Fri Jun 22 16:50:45 2018 +0200
    
        added support for multiple LED strips
    
    commit b5d41cf9d83821127b2e7fcbcd29c1b776ac813e
    Author: Aran Kieskamp <aranna00@gmail.com>
    Date:   Fri Jun 22 16:55:00 2018 +0200
    
        updated arduino sketch with support for 3 LED strips
    
    commit 1a59b12cc6d641f7fcd72177868146a567e9b486
    Author: Aran Kieskamp <aranna00@gmail.com>
    Date:   Fri Jun 22 16:50:45 2018 +0200
    
        added support for multiple LED strips

--- src/modules/led-strip-controller/sketches/sketch_led_controller/sketch_led_controller.ino
+++ src/modules/led-strip-controller/sketches/sketch_led_controller/sketch_led_controller.ino
@@ -4,13 +4,16 @@
 FASTLED_USING_NAMESPACE
 
 #define DATA_PIN 13
+#define DATA_PIN_2 12
+#define DATA_PIN_3 11
+#define ACTIVE_PLACE 13
 #define ADDRESS 0x40
-#define LED_TYPE UCS1903B
-#define COLOR_ORDER RGB
-#define NUM_LEDS 5
+#define LED_TYPE WS2812B
+#define COLOR_ORDER GRB
+#define NUM_LEDS 60
 #define BRIGHTNESS 255/1
 #define FRAMES_PER_SECOND 60
-#define HUE_OFFSET 4
+#define HUE_OFFSET 2
 
 enum class LightingType : uint8_t {
     SPECIFIC = 0,
@@ -26,6 +29,7 @@ enum class ColorType : uint8_t {
 struct LedStatus {
     LightingType lightingType;
     ColorType colorType;
+    uint8_t place;
 };
 
 struct SpecificColor {
@@ -57,28 +61,44 @@ struct Circle {
 
 CRGB leds[NUM_LEDS];
 CHSV hsv[NUM_LEDS];
+CRGB leds2[NUM_LEDS];
+CHSV hsv2[NUM_LEDS];
+CRGB leds3[NUM_LEDS];
+CHSV hsv3[NUM_LEDS];
 byte start_led;
 byte end_led;
 bool rbcw = false;
 bool cyclecw = false;
 bool rainbow = false;
 bool cycle = false;
+bool cycle2 = false;
+bool cycle3 = false;
 bool individual = true;
 bool rainbowLEDS[NUM_LEDS];
+bool rainbowLEDS2[NUM_LEDS];
+bool rainbowLEDS3[NUM_LEDS];
 uint8_t gHue = 0;
 int ledId = 0;
+int ledId2 = 0;
+int ledId3 = 0;
 Circle circleInfo;
+Circle circleInfo2;
+Circle circleInfo3;
 
 void setup() {
     Wire.begin(ADDRESS);
     Wire.onReceive(on_receive_message);
 
-    FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(Typical8mmPixel);
+    FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(UncorrectedColor).setTemperature(UncorrectedTemperature);
+    FastLED.addLeds<LED_TYPE, DATA_PIN_2, COLOR_ORDER>(leds2, NUM_LEDS).setCorrection(UncorrectedColor).setTemperature(UncorrectedTemperature);
+    FastLED.addLeds<LED_TYPE, DATA_PIN_3, COLOR_ORDER>(leds3, NUM_LEDS).setCorrection(UncorrectedColor).setTemperature(UncorrectedTemperature);
     FastLED.setBrightness(BRIGHTNESS);
 }
 
 void loop() {
     CRGB rgbVals = CRGB(0, 0, 0);
+    CRGB rgbVals2 = CRGB(0, 0, 0);
+    CRGB rgbVals3 = CRGB(0, 0, 0);
     for (int i = 0; i < NUM_LEDS; ++i) {
         if (individual) {
             if (rainbowLEDS[i]) {
@@ -96,19 +116,66 @@ void loop() {
         }
     }
 
+    for (int i = 0; i < NUM_LEDS; ++i) {
+        if (individual) {
+            if (rainbowLEDS2[i]) {
+                if (rbcw) {
+                    hsv2[i] = CHSV(hsv2[i].h - HUE_OFFSET, hsv2[i].s == 0 ? 255 : hsv2[i].s,
+                                   hsv2[i].v == 0 ? 255 : hsv2[i].s);
+                } else {
+                    hsv2[i] = CHSV(hsv2[i].h + HUE_OFFSET, hsv2[i].s == 0 ? 255 : hsv2[i].s,
+                                   hsv2[i].v == 0 ? 255 : hsv2[i].s);
+                }
+            }
+            leds2[i] = hsv2[i];
+        } else {
+            leds2[i] = CHSV(0, 0, 0);
+        }
+    }
+
+    for (int i = 0; i < NUM_LEDS; ++i) {
+        if (individual) {
+            if (rainbowLEDS3[i]) {
+                if (rbcw) {
+                    hsv3[i] = CHSV(hsv3[i].h - HUE_OFFSET, hsv3[i].s == 0 ? 255 : hsv3[i].s,
+                                   hsv3[i].v == 0 ? 255 : hsv3[i].s);
+                } else {
+                    hsv3[i] = CHSV(hsv3[i].h + HUE_OFFSET, hsv3[i].s == 0 ? 255 : hsv3[i].s,
+                                   hsv3[i].v == 0 ? 255 : hsv3[i].s);
+                }
+            }
+            leds3[i] = hsv3[i];
+        } else {
+            leds3[i] = CHSV(0, 0, 0);
+        }
+    }
+
     if (rainbow) {
         hsv2rgb_rainbow(CHSV(gHue, 255, 255), rgbVals);
         for (int j = 0; j < NUM_LEDS; ++j) {
             leds[j] += rgbVals;
+            leds2[j] += rgbVals;
+            leds3[j] += rgbVals;
         }
         gHue++;
     }
 
     if (cycle) {
         hsv2rgb_rainbow(CHSV(circleInfo.hue, circleInfo.saturation, 255), rgbVals);
+        if(ledId-1>=0){
+            leds[ledId-1] = rgbVals;
+        }
+        else{
+            leds[NUM_LEDS-1] = rgbVals;
+        }
         leds[ledId] = rgbVals;
-        EVERY_N_MILLISECONDS(250)
-        {
+        if(ledId+1<=NUM_LEDS-1){
+            leds[ledId+1] = rgbVals;
+        }
+        else{
+            leds[0] = rgbVals;
+        }
+        EVERY_N_MILLISECONDS(250) {
             if (circleInfo.cw) {
                 if (ledId < circleInfo.endId) {
                     ledId++;
@@ -120,6 +187,70 @@ void loop() {
                     ledId--;
                 } else {
                     ledId = circleInfo.endId;
+                }
+            }
+        }
+    }
+
+    if (cycle2) {
+        hsv2rgb_rainbow(CHSV(circleInfo2.hue, circleInfo2.saturation, 255), rgbVals);
+        if(ledId2-1>=0){
+            leds2[ledId-1] = rgbVals;
+        }
+        else{
+            leds2[NUM_LEDS-1] = rgbVals;
+        }
+        leds2[ledId2] = rgbVals;
+        if(ledId+1<=NUM_LEDS-1){
+            leds2[ledId2+1] = rgbVals;
+        }
+        else{
+            leds2[0] = rgbVals;
+        }
+        EVERY_N_MILLISECONDS(250) {
+            if (circleInfo2.cw) {
+                if (ledId2 < circleInfo2.endId) {
+                    ledId2++;
+                } else {
+                    ledId2 = circleInfo2.startId;
+                }
+            } else {
+                if (ledId2 > circleInfo2.startId) {
+                    ledId2--;
+                } else {
+                    ledId2 = circleInfo2.endId;
+                }
+            }
+        }
+    }
+
+    if (cycle3) {
+        hsv2rgb_rainbow(CHSV(circleInfo3.hue, circleInfo3.saturation, 255), rgbVals);
+        if(ledId3-1>=0){
+            leds3[ledId-1] = rgbVals;
+        }
+        else{
+            leds3[NUM_LEDS-1] = rgbVals;
+        }
+        leds3[ledId] = rgbVals;
+        if(ledId3+1<=NUM_LEDS-1){
+            leds3[ledId3+1] = rgbVals;
+        }
+        else{
+            leds3[0] = rgbVals;
+        }
+        EVERY_N_MILLISECONDS(250) {
+            if (circleInfo3.cw) {
+                if (ledId3 < circleInfo3.endId) {
+                    ledId3++;
+                } else {
+                    ledId3 = circleInfo3.startId;
+                }
+            } else {
+                if (ledId3 > circleInfo3.startId) {
+                    ledId3--;
+                } else {
+                    ledId3 = circleInfo3.endId;
                 }
             }
         }
@@ -145,6 +276,7 @@ void on_receive_message(int length) {
 
     buffer[0] = Wire.read();
     buffer[1] = Wire.read();
+    buffer[2] = Wire.read();
 
     LedStatus *packetType = (LedStatus*) buffer;
 
@@ -155,17 +287,40 @@ void on_receive_message(int length) {
             for (int i = 0; Wire.available(); i++) {
                 ledBuffer[i] = Wire.read();
             }
-            SpecificColor *command = (SpecificColor*) ledBuffer;
+
+            SpecificColor* command = (SpecificColor*) ledBuffer;
             rainbowLEDS[command->ledId] = false;
-            hsv[command->ledId] = CHSV(command->hue, command->saturation, command->value);
+            switch(packetType->place){
+                case 0:
+                    hsv[command->ledId] = CHSV(command->hue, command->saturation, command->value);
+                    break;
+                case 1:
+                    hsv2[command->ledId] = CHSV(command->hue, command->saturation, command->value);
+                    break;
+                case 2:
+                    hsv3[command->ledId] = CHSV(command->hue, command->saturation, command->value);
+                    break;
+            }
         } else if (packetType->colorType == ColorType::RAINBOW) {
             unsigned char ledBuffer[sizeof(SpecificRainbow)];
             for (int i = 0; Wire.available(); i++) {
                 ledBuffer[i] = Wire.read();
             }
-            SpecificRainbow *command = (SpecificRainbow*) ledBuffer;
-            hsv[command->ledId] = CHSV(command->startHue, 255, 255);
-            rainbowLEDS[command->ledId] = true;
+            SpecificRainbow* command = (SpecificRainbow*) ledBuffer;
+            switch(packetType->place){
+                case 0:
+                    hsv[command->ledId] = CHSV(command->startHue, 255, 255);
+                    rainbowLEDS[command->ledId] = true;
+                    break;
+                case 1:
+                    hsv2[command->ledId] = CHSV(command->startHue, 255, 255);
+                    rainbowLEDS2[command->ledId] = true;
+                    break;
+                case 2:
+                    hsv3[command->ledId] = CHSV(command->startHue, 255, 255);
+                    rainbowLEDS3[command->ledId] = true;
+                    break;
+            }
         }
     } else if (packetType->lightingType == LightingType::CIRCLE) {
         rainbow = false;
@@ -178,12 +333,32 @@ void on_receive_message(int length) {
         if (command->startId == command->endId) {
             cycle = false;
         } else {
-            circleInfo.startId = command->startId;
-            circleInfo.endId = command->endId;
-            circleInfo.cw = command->cw;
-            circleInfo.hue = command->hue;
-            circleInfo.saturation = command->saturation;
-            cycle = true;
+            switch(packetType->place){
+                case 0:
+                    circleInfo.startId = command->startId;
+                    circleInfo.endId = command->endId;
+                    circleInfo.cw = command->cw;
+                    circleInfo.hue = command->hue;
+                    circleInfo.saturation = command->saturation;
+                    cycle = true;
+                    break;
+                case 1:
+                    circleInfo2.startId = command->startId;
+                    circleInfo2.endId = command->endId;
+                    circleInfo2.cw = command->cw;
+                    circleInfo2.hue = command->hue;
+                    circleInfo2.saturation = command->saturation;
+                    cycle2 = true;
+                    break;
+                case 2:
+                    circleInfo3.startId = command->startId;
+                    circleInfo3.endId = command->endId;
+                    circleInfo3.cw = command->cw;
+                    circleInfo3.hue = command->hue;
+                    circleInfo3.saturation = command->saturation;
+                    cycle3 = true;
+                    break;
+            }
         }
     } else if (packetType->lightingType == LightingType::ALL) {
         unsigned char ledBuffer[sizeof(AllLeds)];
@@ -195,8 +370,22 @@ void on_receive_message(int length) {
             rainbow = true;
         } else {
             rainbow = false;
-            for (int i = 0; i < NUM_LEDS; ++i) {
-                hsv[i] = CHSV(command->hue, command->saturation, command->value);
+            switch(packetType->place){
+                case 0:
+                    for (int i = 0; i < NUM_LEDS; ++i) {
+                        hsv[i] = CHSV(command->hue, command->saturation, command->value);
+                    }
+                    break;
+                case 1:
+                    for (int i = 0; i < NUM_LEDS; ++i) {
+                        hsv2[i] = CHSV(command->hue, command->saturation, command->value);
+                    }
+                    break;
+                case 2:
+                    for (int i = 0; i < NUM_LEDS; ++i) {
+                        hsv3[i] = CHSV(command->hue, command->saturation, command->value);
+                    }
+                    break;
             }
         }
     }
